a714765fb263aa5c9aa6e36a55753cc730085539
NealRame <contact@nealrame.com>
2016-09-19 14:59:52 +0200

    livereload in da place

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,10 @@
 const gulp = require('gulp');
+const livereload = require('gulp-livereload');
 const metalsmith = require('gulp-metalsmith');
+
 const markdown = require('metalsmith-markdown');
 const layouts = require('metalsmith-layouts');
+
 const del = require('del');
 const path = require('path');
 
@@ -16,7 +19,7 @@ const build_dir = process.env.BUILD_OUTPUT_DIR || path.join(__dirname, 'build');
 
 gulp.task('default', ['metalsmith']);
 gulp.task('clean', () => del(path.join(build_dir, '**')));
-gulp.task('watch', ['watch-content']);
+gulp.task('watch', ['watch-content'], () => livereload.listen());
 gulp.task('watch-content', () => gulp.watch(
 	[
 		path.join(content_dir, '**'),
@@ -36,4 +39,5 @@ gulp.task('metalsmith', ['clean'], () => gulp.src(path.join(content_dir, '**'))
 		]
 	}))
 	.pipe(gulp.dest(build_dir))
+	.pipe(livereload())
 );
