22dda3d4ffd6cdadc862180ddcf59d52164d5a24
NealRame <contact@nealrame.com>
2016-09-26 18:24:00 +0200

    add each page the main style

--- tools/gulp/tasks/content.js
+++ tools/gulp/tasks/content.js
@@ -5,43 +5,55 @@ const livereload = require('gulp-livereload');
 const metalsmith = require('gulp-metalsmith');
 const htmlmin = require('gulp-htmlmin');
 
-const discover_hbs_helpers = require('metalsmith/discover-hbs-helpers');
 const layouts = require('metalsmith-layouts');
 const markdown = require('metalsmith-markdown');
-
-const env = require('gulp/env').content;
+const discover_hbs_helpers = require('metalsmith/discover-hbs-helpers');
+const each = require('metalsmith/each');
 
 const del = require('del');
 const path = require('path');
+const uniq = require('lodash/uniq');
+const {cat} = require('core/functional');
+
+const env = require('gulp/env');
+const content_env = env.content;
 
 gulp
-	.task('content-clean', [], () => del(path.join(env.outputDir, '**/.html')))
-	.task('content', ['content-clean'], () => gulp.src(path.join(env.matterDir, '**'))
+	.task('content-clean', [], () => del(path.join(content_env.outputDir, '**/.html')))
+	.task('content', ['content-clean'], () => gulp.src(path.join(content_env.matterDir, '**'))
 		.pipe(metalsmith({
-			metadata: env.metadata,
+			metadata: content_env.metadata,
 			use: [
 				discover_hbs_helpers({
-					directory: env.helpersDir
+					directory: content_env.helpersDir
+				}),
+				each({
+					iteratee: (file, metadata, next) => {
+						metadata.styles =
+							uniq(cat(['style'], metadata.styles || []))
+								.map(style => env.sass.prefix(style));
+						next();
+					}
 				}),
 				markdown(),
 				layouts({
-					directory: env.layoutsDir,
-					partials: env.partialsDir,
+					directory: content_env.layoutsDir,
+					partials: content_env.partialsDir,
 					engine: 'handlebars',
 					default: 'index.hbs'
 				})
 			]
 		}))
 		.pipe(htmlmin({collapseWhitespace: true}))
-		.pipe(gulp.dest(env.outputDir))
+		.pipe(gulp.dest(content_env.outputDir))
 		.pipe(livereload())
 	)
 	.task('content-watch', ['content'], () => gulp.watch(
 		[
-			path.join(env.matterDir, '**'),
-			path.join(env.layoutsDir, '**/*.hbs'),
-			path.join(env.partialsDir, '**/*.hbs'),
-			path.join(env.helpersDir, '**/*.js')
+			path.join(content_env.matterDir, '**'),
+			path.join(content_env.layoutsDir, '**/*.hbs'),
+			path.join(content_env.partialsDir, '**/*.hbs'),
+			path.join(content_env.helpersDir, '**/*.js')
 		],
 		['content']
 	));
