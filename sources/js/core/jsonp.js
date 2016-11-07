9067b4da82066dfd95de0b3638c407c06f192ec6
NealRame <contact@nealrame.com>
2016-11-07 23:46:50 +0100

    add jsonp core module

---
+++ sources/js/core/jsonp.js
@@ -0,0 +1,10 @@
+export default function jsonp(url, callback_name, callback) {
+	const body = document.getElementsByTagName('body')[0];
+	const script = document.createElement('script');
+
+	script.type = 'text/javascript';
+	script.src  = url;
+
+	body.appendChild(script);
+	window[callback_name] = callback;
+}
